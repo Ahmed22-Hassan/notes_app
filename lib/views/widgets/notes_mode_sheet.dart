566f9b1f4b6fc8d7e6fa3ee81b915cd2073dd5e2
Ahmed22_Hassan <ahmeddd.hassan258@gmail.com>
2024-11-25 02:25:21 +0200

    create notesmodesheet

---
+++ lib/views/widgets/notes_mode_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesModeSheet extends StatelessWidget {
+  const NotesModeSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

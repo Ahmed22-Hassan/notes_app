38b2e8cf323a806eba3c96fb15ba5f0d7559f991
Ahmed22_Hassan <ahmeddd.hassan258@gmail.com>
2024-11-27 00:40:40 +0200

    create note model

---
+++ lib/models/model_note.dart
@@ -0,0 +1,12 @@
+class ModelNote {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  ModelNote(
+      {required this.subtitle,
+      required this.date,
+      required this.color,
+      required this.title});
+}
